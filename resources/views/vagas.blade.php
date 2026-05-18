0ef5c9ed047ff7b03b6d4198c1a4dbcde1732b2c
jose <daftwarz@gmail.com>
2026-05-17 21:38:12 -0300

    ajustes

---
+++ resources/views/vagas.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+@section('content')
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <div class="card shadow-sm">
+                <div class="card-body p-4">
+                    <h2 class="card-title text-center mb-4">Vagas</h2>
